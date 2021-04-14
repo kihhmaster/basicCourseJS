b5c82165779c2927f7b2eabf0370f00ae6cb4799
Kihhmaster <kihhmaster@yandex.ru>
2021-04-14 11:51:27 +0300

    Lesson_8 finished

---
+++ lesson_8/1 текст вместо картинки/app.js
@@ -0,0 +1,89 @@
+'use strict';
+// 1. получите все кнопки и сохраните в переменную
+let button = document.querySelectorAll('button');
+// 1.1 затем проитерируйтесь по кнопкам и каждой из
+// них добавьте обработчик клика - функцию handleClick
+button.forEach(item => {
+	item.addEventListener('click', handleClick)
+	
+});
+
+/**
+ * Функция обрабатывает клик по кнопке в карточке товара и попеременно вызывает
+ * функции для показа или скрытия текста о товаре.
+ * @param {MouseEvent} clickedButtonEvent 
+ */
+function handleClick(clickedButtonEvent) {
+    // 2. из объекта события получите ссылку на .product и
+    // сохраните в переменную:
+    const cardNode = clickedButtonEvent.target.parentElement;
+
+    // 3. создайте литерал объекта со следующими свойствами:
+    const card = {
+        wrap: cardNode, // здесь элемент с классом .product
+        img: cardNode.children[1], // здесь картинка внутри .product
+        productName: cardNode.children[0], // здесь .productName, который внутри .product
+        button: cardNode.children[2], // здесь button, который внутри .product
+    };
+
+    // 4. получаем текст на кнопке, которая внутри .product
+    
+    
+    if (card.button.textContent == "Подробнее") { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
+        // 4.2 если да, то передаем объект card в функцию showMoreText
+        showMoreText(card);
+    } else if (card.button.textContent == "Отмена") { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
+        // 4.4 если да, то передаем объект card в функцию hideMoreText
+        hideMoreText(card);
+    }
+}
+
+/**
+ * Функция скрывает текст с описанием товара.
+ * @param {Object} card 
+ * @param {HTMLDivElement} card.wrap
+ * @param {HTMLImageElement} card.img
+ * @param {HTMLDivElement} card.productName
+ * @param {HTMLButtonElement} card.button
+ */
+function hideMoreText(card) {
+    // 5. картинке внутри .product ставим стиль display: block
+    card.img.style.display = 'block'
+    // 5.1 внутри .product находим элемент с классом .desc и удаляем его
+    let delDesc = document.querySelector('.desc');
+		delDesc.remove();
+    // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
+    card.button.textContent = "Подробнее";
+}
+
+/**
+ * Функция показывает текст с описанием товара.
+ * @param {Object} card 
+ * @param {HTMLDivElement} card.wrap
+ * @param {HTMLImageElement} card.img
+ * @param {HTMLDivElement} card.productName
+ * @param {HTMLButtonElement} card.button 
+ */
+function showMoreText(card) {
+    // 6. картинке внутри .product ставим display: none
+    card.img.style.display = 'none';
+    // 6.1 сохраняем произвольный текст в переменную
+    let loremText = 'Сделайте, чтобы при клике по кнопке, вместо картинки над этой кнопкой появлялся какой-нибудь текст.';
+    // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
+
+// ВОПРОС?????????????????
+		// card.productName.insertAdjacentHTML('afterend', `<div class="desc">${loremText}<div>`); 
+		// Не могу понять почему если вставляешь после card.productName перекращается выполнение
+												// let delDesc = document.querySelector('.desc');
+												// // delDesc.remove();
+												// card.button.textContent = "Подробнее"; 
+
+
+
+		card.button.insertAdjacentHTML('afterend', `<div class="desc">${loremText}<div>`);
+    // 6.3 внутри .product у кнопки текст ставим "Отмена"
+    card.button.textContent = "Отмена";
+}
+
+
+
